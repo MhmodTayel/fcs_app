e7ff612eca62f1acc37903f43a597ebe5080b19b
Mahmoud Mohamed Tayel <mhmodtayel0050@gmail.com>
2024-07-28 18:38:52 +0300

    first commit

---
+++ fcs_app/fcs_custom_app/doctype/testdoctypeformigration/test_testdoctypeformigration.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Mahmoud Tayel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TesttestDocTypeForMigration(FrappeTestCase):
+	pass
